d04a220bbb36ee4ebcce3e326ce46f94b2574dc1
hxllrxiser <bvital374@gmail.com>
2025-03-04 12:51:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+def multiply(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Calculator")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    operation = input("Choose operation (+ or *): ")
+    if operation == "+":
+        result = add(num1, num2)
+    elif operation == "*":
+        result = multiply(num1, num2)
+    else: 
+        print("Invalid operation")
+        exit()
+    print(f"Result: {result}")
